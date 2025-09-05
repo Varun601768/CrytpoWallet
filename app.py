95ad84da19122dedc5841854c68c6c7c1345cace
Varun M C <varunmcchinthu@gmail.com>
2025-09-05 20:34:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -666,7 +666,7 @@ def home():
 @app.route('/analysis')
 @login_required
 def analysis():
-    return render_template('analysis.html')
+    return render_template('analysis.html',result="Test value")
 
 @app.route('/con')
 @login_required
